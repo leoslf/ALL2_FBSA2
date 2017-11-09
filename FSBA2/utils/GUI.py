5bb644b168b22c9ac3cce2b56f98cb0d1d7a55f4
leoslf <lfsin3-c@my.cityu.edu.hk>
2017-11-09 22:57:32 +0800

    Add files via upload

--- FSBA2/utils/GUI.py
+++ FSBA2/utils/GUI.py
@@ -166,7 +166,8 @@ class GUI(tk.Frame):
             self.width = width
             self.height = int(root.winfo_height() * 0.075)
             tk.Frame.__init__(self, root, height=self.height)
-            canvas = self.GradientCanvas(self, (68,68,68), (32,32,32), self.height)
+            #canvas = self.GradientCanvas(self, (68,68,68), (32,32,32), self.height)
+            canvas = self.GradientCanvas(self, (0, 198, 251), (0,91,234), self.height, True)
             tk.Label(canvas, text=app_name)
             #lbls = [tk.Label(canvas, text=s, fg="white", bg="green") for s in (app_name, displayed_username)]
             lbls = [tk.Label(canvas, text=s, fg="white", bg="systemTransparent", font=("Helvetica", 20)) for s in (app_name, displayed_username)]
@@ -185,7 +186,7 @@ class GUI(tk.Frame):
 
             The Gradient background, supporting widget of LogoBar
             """
-            def __init__(self, root, color1, color2, height, bd=0, relief="sunken"):
+            def __init__(self, root, color1, color2, height, horizontal=False, bd=0, relief="sunken"):
                 """__init__
 
                 Parameters
@@ -199,7 +200,7 @@ class GUI(tk.Frame):
                 color2 : 3-tuple containing rgb value
                     second color
 
-                borderwidth : int 
+                bd : int 
                     (inherited) 
 
                 relief: str
@@ -213,7 +214,9 @@ class GUI(tk.Frame):
                 self.color2 = color2
                 self.colors = [color1, color2]
                 self.height = height
+                self.horizontal = horizontal
                 self.bind("<Configure>", self.draw_gradient)
+                self.draw_gradient()
                 #self.grid(row=0, column=0, sticky="nsew")
                 #self.pack(fill="both", expand=True)
                 self.pack(fill=tk.X)#, expand=True)
@@ -228,19 +231,35 @@ class GUI(tk.Frame):
                 """
                 self.delete("gradient")
                 width = self.winfo_width()
-                height = self.height #self.winfo_height()
-                limit = height
-                #(r1,g1,b1) = self.winfo_rgb(self.color1)
-                #(r2,g2,b2) = self.winfo_rgb(self.color2)
-                #TODO colors = list(map(self.winfo_rgb, self.colors))
-                colors = self.colors
-                ratios = list(map(lambda comp: float(comp[1] - comp[0])/limit, zip(*colors)))
-                
-                for i in range(limit):
-                    ncolors = list(map(lambda tup: int(tup[0] + (tup[1] * i)), zip(colors[0], ratios)))
-                    color = "#%4.4x%4.4x%4.4x" % tuple(ncolors)
-                    self.create_line(0, i, width, i, tags=("gradient",), fill=color)
-                self.lower("gradient")
+                height = self.height
+
+                if self.horizontal:
+                    limit = height
+                    colors = self.colors
+                    ratios = list(map(lambda comp: float(comp[1] - comp[0])/limit, zip(*colors)))
+                    
+                    for i in range(limit):
+                        ncolors = list(map(lambda tup: int(tup[0] + (tup[1] * i)), zip(colors[0], ratios)))
+                        debug("ncolors: %r" % ncolors)
+                        #color = "#%4.4x%4.4x%4.4x" % tuple(ncolors)
+                        color = "#%02x%02x%02x" % tuple(ncolors)
+                        debug("color: %r" % color)
+                        self.create_line((0, i, width, i + 1), tags=("gradient",), fill=color)
+                    self.lower("gradient")
+                else:
+                    limit = width
+                    colors = self.colors
+                    ratios = list(map(lambda comp: float(comp[1] - comp[0])/limit, zip(*colors)))
+                    
+                    for i in range(limit):
+                        ncolors = list(map(lambda tup: int(tup[0] + (tup[1] * i)), zip(colors[0], ratios)))
+                        debug("ncolors: %r" % ncolors)
+                        #color = "#%4.4x%4.4x%4.4x" % tuple(ncolors)
+                        color = "#%02x%02x%02x" % tuple(ncolors)
+                        debug("color: %r" % color)
+                        self.create_line((i, 0, i + 1, height), tags=("gradient",), fill=color)
+                    self.lower("gradient")
+
             
 
     def moveToTop(self):
